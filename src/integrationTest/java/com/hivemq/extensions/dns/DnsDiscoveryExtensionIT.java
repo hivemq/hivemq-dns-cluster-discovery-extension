22d4e9da392c9c1bf85a8938d7347af7d1c6f2a1
Silvio Giebl <silvio.giebl@hivemq.com>
2021-11-06 21:56:13 +0100

    Update testcontainers, improve DnsDiscoveryExtensionIT

--- src/integrationTest/java/com/hivemq/extensions/dns/DnsDiscoveryExtensionIT.java
+++ src/integrationTest/java/com/hivemq/extensions/dns/DnsDiscoveryExtensionIT.java
@@ -17,20 +17,19 @@ package com.hivemq.extensions.dns;
 
 import com.hivemq.extension.sdk.api.annotations.NotNull;
 import com.hivemq.testcontainer.junit5.HiveMQTestContainerExtension;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.io.TempDir;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.Network;
-import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.shaded.okhttp3.OkHttpClient;
 import org.testcontainers.shaded.okhttp3.Request;
 import org.testcontainers.shaded.okhttp3.Response;
-import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
+import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -41,19 +40,19 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import static com.hivemq.extensions.dns.metrics.DnsDiscoveryMetrics.DNS_DISCOVERY_EXTENSION;
-import static com.hivemq.extensions.dns.metrics.DnsDiscoveryMetrics.HIVEMQ_PREFIX;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author Lukas Brand
  */
-public class DnsDiscoveryExtensionIT {
+class DnsDiscoveryExtensionIT {
 
     private static final @NotNull Logger log = LoggerFactory.getLogger(DnsDiscoveryExtensionIT.class);
 
-    private final @NotNull Network network = Network.newNetwork();
+    private static final @NotNull String SUCCESS_METRIC = "com_hivemq_dns_cluster_discovery_extension_query_success_count";
+    private static final @NotNull String FAILURE_METRIC = "com_hivemq_dns_cluster_discovery_extension_query_failed_count";
+    private static final @NotNull String IP_COUNT_METRIC = "com_hivemq_dns_cluster_discovery_extension_resolved_addresses";
 
     private @NotNull TestDnsServer testDnsServer;
     private @NotNull HiveMQTestContainerExtension node1;
@@ -63,40 +62,37 @@ public class DnsDiscoveryExtensionIT {
         testDnsServer = new TestDnsServer(Set.of("tasks.hivemq"), 4);
         testDnsServer.start();
 
-        FileUtils.copyDirectory(new File("build/hivemq-extension-test"), extensionTempPath.toFile());
-        final Path extensionDir = extensionTempPath.resolve("hivemq-dns-cluster-discovery");
-
-        final String dnsConfigPath = "src/integrationTest/resources/dnsdiscovery.properties";
-
-        final String replacedConfig = Files.readString(Path.of(dnsConfigPath))
+        final Path dnsConfigFile = extensionTempPath.resolve("dnsdiscovery.properties");
+        final String replacedConfig = Files.readString(Path.of(getClass().getResource("/dnsdiscovery.properties").getPath()))
                 .replace("dnsServerPlaceholder", "host.docker.internal" + ":" + testDnsServer.localAddress().getPort())
                 .replace("discoveryPlaceholder", "tasks.hivemq");
-        Files.writeString(extensionDir.resolve("dnsdiscovery.properties"), replacedConfig, StandardOpenOption.CREATE);
+        Files.writeString(dnsConfigFile, replacedConfig, StandardOpenOption.CREATE);
 
-        node1 = new HiveMQTestContainerExtension("hivemq/hivemq4", "latest")
-                .withHiveMQConfig(new File("src/integrationTest/resources/config.xml"))
-                .withExtension(extensionDir.toFile())
-                .withExtension(new File("src/integrationTest/resources/hivemq-prometheus-extension"))
-                .withNetwork(network)
+        node1 = new HiveMQTestContainerExtension(DockerImageName.parse("hivemq/hivemq4").withTag("latest"))
+                .withHiveMQConfig(MountableFile.forClasspathResource("config.xml"))
+                .withExtension(MountableFile.forClasspathResource("hivemq-dns-cluster-discovery"))
+                .withFileInExtensionHomeFolder(MountableFile.forHostPath(dnsConfigFile), "hivemq-dns-cluster-discovery")
+                .withExtension(MountableFile.forClasspathResource("hivemq-prometheus-extension"))
                 //.withEnv("HIVEMQ_CLUSTER_TRANSPORT_TYPE", "TCP")
                 .withNetworkAliases("node1")
-                .withExposedPorts(9399)
-                .waitingFor(
-                        Wait.forLogMessage(".*Started HiveMQ in.*\\n", 1)
-                );
+                .withExposedPorts(9399);
     }
 
+    @AfterEach
+    void tearDown() {
+        node1.stop();
+        testDnsServer.stop();
+    }
 
     @Test
     @Timeout(value = 2, unit = TimeUnit.MINUTES)
-    void test_metric_success() throws IOException, InterruptedException {
+    void test_metric_success() throws IOException {
         node1.start();
 
-        assertEquals(1, getMetricValue(MetricType.SUCCESS_METRIC));
-        assertEquals(0, getMetricValue(MetricType.FAILURE_METRIC));
-        assertEquals(4, getMetricValue(MetricType.IP_COUNT_METRIC));
-
-
+        final Map<String, Float> metrics = getMetrics();
+        assertEquals(1, metrics.get(SUCCESS_METRIC));
+        assertEquals(0, metrics.get(FAILURE_METRIC));
+        assertEquals(4, metrics.get(IP_COUNT_METRIC));
     }
 
     @Test
@@ -105,19 +101,13 @@ public class DnsDiscoveryExtensionIT {
         testDnsServer.stop();
         node1.start();
 
-        assertEquals(0, getMetricValue(MetricType.SUCCESS_METRIC));
-        assertEquals(1, getMetricValue(MetricType.FAILURE_METRIC));
-        assertEquals(0, getMetricValue(MetricType.IP_COUNT_METRIC));
+        final Map<String, Float> metrics = getMetrics();
+        assertEquals(0, metrics.get(SUCCESS_METRIC));
+        assertEquals(1, metrics.get(FAILURE_METRIC));
+        assertEquals(0, metrics.get(IP_COUNT_METRIC));
     }
 
-
-    private enum MetricType {
-        SUCCESS_METRIC,
-        FAILURE_METRIC,
-        IP_COUNT_METRIC
-    }
-
-    private @NotNull Float getMetricValue(final @NotNull MetricType type) throws IOException {
+    private @NotNull Map<String, Float> getMetrics() throws IOException {
 
         final OkHttpClient client = new OkHttpClient();
         final Request request1 = new Request.Builder()
@@ -128,26 +118,7 @@ public class DnsDiscoveryExtensionIT {
         assertNotNull(response1.body());
         final String string = response1.body().string();
 
-        final Map<String, Float> metricsWithValues = parseMetrics(string,
-                Set.of("com_hivemq_dns_cluster_discovery_extension_query_success_count",
-                        "com_hivemq_dns_cluster_discovery_extension_query_failed_count",
-                        "com_hivemq_dns_cluster_discovery_extension_resolved_addresses"));
-
-        final String successMetricName = HIVEMQ_PREFIX + "." +  DNS_DISCOVERY_EXTENSION + "." +  "query.success.count";
-        final String failedMetricName = HIVEMQ_PREFIX + "." +  DNS_DISCOVERY_EXTENSION + "." + "query.failed.count";
-
-
-
-        if (type == MetricType.SUCCESS_METRIC) {
-            return metricsWithValues.get("com_hivemq_dns_cluster_discovery_extension_query_success_count");
-        } else if (type == MetricType.FAILURE_METRIC) {
-            return metricsWithValues.get("com_hivemq_dns_cluster_discovery_extension_query_failed_count");
-        } else if (type == MetricType.IP_COUNT_METRIC) {
-            return metricsWithValues.get("com_hivemq_dns_cluster_discovery_extension_resolved_addresses");
-        } else {
-            return Float.NaN;
-        }
-
+        return parseMetrics(string, Set.of(SUCCESS_METRIC, FAILURE_METRIC, IP_COUNT_METRIC));
     }
 
     private @NotNull Map<String, Float> parseMetrics(
@@ -159,9 +130,6 @@ public class DnsDiscoveryExtensionIT {
                 .filter(splits -> metrics.contains(splits[0]))
                 .peek(strings -> log.info(Arrays.toString(strings)))
                 .map(splits -> Map.entry(splits[0], Float.parseFloat(splits[1])))
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        Float::max));
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, Float::max));
     }
 }
