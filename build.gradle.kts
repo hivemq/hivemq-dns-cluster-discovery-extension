c8b91a0250e1a2407b596a776228788ffe63dd8b
Silvio Giebl <silvio.giebl@hivemq.com>
2025-02-04 09:46:55 +0100

    Update gradle-oci to 0.21.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,17 +44,15 @@ oci {
                 dependencies {
                     runtime("com.hivemq:hivemq-enterprise:latest") { isChanging = true }
                 }
-                layers {
-                    layer("main") {
-                        contents {
-                            permissions("opt/hivemq/", 0b111_111_101)
-                            permissions("opt/hivemq/extensions/", 0b111_111_101)
-                            into("opt/hivemq/extensions") {
-                                permissions("*/", 0b111_111_101)
-                                permissions("*/dnsdiscovery.properties", 0b110_110_100)
-                                permissions("*/hivemq-extension.xml", 0b110_110_100)
-                                from(zipTree(tasks.hivemqExtensionZip.flatMap { it.archiveFile }))
-                            }
+                layer("main") {
+                    contents {
+                        permissions("opt/hivemq/", 0b111_111_101)
+                        permissions("opt/hivemq/extensions/", 0b111_111_101)
+                        into("opt/hivemq/extensions") {
+                            permissions("*/", 0b111_111_101)
+                            permissions("*/dnsdiscovery.properties", 0b110_110_100)
+                            permissions("*/hivemq-extension.xml", 0b110_110_100)
+                            from(zipTree(tasks.hivemqExtensionZip.flatMap { it.archiveFile }))
                         }
                     }
                 }
